dec82c4cc908b1a99529c4b4ec0fcd66ebb45c18
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-09-14 15:25:25 +0530

    smooth scroll 2 d top & output===floor(score)

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ form.addEventListener("submit", (e) => {
     let score = 0;
 
     // collecting the answers the user will give..value here means A,B,C,D that we had set 
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value,]
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
 
-    // looping thru the userAnswers
+    // looping thru the userAnswers..index so that correctAnswer and userAnswer both match and the 'answer' is just name we gave for a given value in the userAnswers
     userAnswers.forEach((answer,index) => {
         if(answer === correctAnswers[index]){
             score += 10;
@@ -25,7 +25,11 @@ form.addEventListener("submit", (e) => {
     });
 
     // show score
-    scrollTo(0,0) // here we mean window.scrollTo but we didnt type window cz its automatically infered and 0,0 means x,y coordinates and we type it first because we want to scroll to the top and then show the result
+    scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    }) // here we mean window.scrollTo but we didnt type window cz its automatically infered and 0,0 means x,y coordinates and we type it first because we want to scroll to the top and then show the result
 
     scoreArea.classList.remove("hidden");
 
@@ -33,7 +37,7 @@ form.addEventListener("submit", (e) => {
     let output = 0;
     const timer = setInterval(() => {
         finalScore.textContent = `${Math.floor(output)}%`;
-        if(output === score){
+        if(output === Math.floor(score)){
             clearInterval(timer);
         }else{
             output++;

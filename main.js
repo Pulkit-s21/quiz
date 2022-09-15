5418a83a7a31c8ab76b9824b7a57ed41c5b7dd5e
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-09-15 19:06:41 +0530

    chngd how mch 2 add 2 score on my own 2 get <=100

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scoreArea = document.querySelector(".scoreArea");
 const finalScore = document.querySelector(".finalScore");
 
 // Array of all the correct answers
-const correctAnswers = ['A','B','C','D','B','A','C','B','A','C'];
+const correctAnswers = ['A','B','C','D','B','A','C','B','A','C','A'];
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -15,12 +15,12 @@ form.addEventListener("submit", (e) => {
     let score = 0;
 
     // collecting the answers the user will give..value here means A,B,C,D that we had set 
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value, form.q11.value];
 
     // looping thru the userAnswers..index so that correctAnswer and userAnswer both match and the 'answer' is just name we gave for a given value in the userAnswers
     userAnswers.forEach((answer,index) => {
         if(answer === correctAnswers[index]){ // we are checking each answer user gives and comparing it with the index of the correctAnswers
-            score += 10;
+            score += 100/correctAnswers.length; // earlier I was doing 100/correctAnswers but I remembered I need to divide it by the length of the correctAnswers (which are the total questions cz we have a crrct ans for every ques and boom it will give % in 0-100 and nvr go above 100 and result can be like 83,22.....)
         }
     });
 
